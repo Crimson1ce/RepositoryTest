c059b828e54a38911e648f011a5c4b215c8faa3c
unknown <m1997maldonado1@gmail.com>
2020-08-06 20:28:56 -0600

    actualizacion

--- P2.PracticaGitHub/src/p2/practicagithub/P2PracticaGitHub.java
+++ P2.PracticaGitHub/src/p2/practicagithub/P2PracticaGitHub.java
@@ -17,8 +17,6 @@ public class P2PracticaGitHub {
     public static void main(String[] args) {
         System.out.println("Hello ladies and gentlemen.");
         System.out.println("No se qué voy a hacer mañana ;-;");
-        System.out.println("Tengo miedo\n"
-                + "tengo miedo");
     }
     
 }
