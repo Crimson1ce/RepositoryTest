2a9a9171193b5c345de6251d2a05959d40e2aefe
Josue <davyd3501@gmail.com>
2020-08-06 20:21:34 -0600

    First push

---
+++ P2.PracticaGitHub/src/p2/practicagithub/P2PracticaGitHub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2.practicagithub;
+
+/**
+ *
+ * @author Josue
+ */
+public class P2PracticaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello ladies and gentlemen.");
+        System.out.println("No se qué voy a hacer mañana ;-;");
+    }
+    
+}
